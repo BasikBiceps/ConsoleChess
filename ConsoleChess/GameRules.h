e1446eadcaf852a73c8ef725fac21ba770dfd0c9
BasikBiceps <nokem.noks@gmail.com>
2020-04-12 21:31:55 +0300

    Added isCastlingPossible declaration.

--- ConsoleChess/GameRules.h
+++ ConsoleChess/GameRules.h
@@ -9,7 +9,6 @@ public:
 	static bool isCheck(GameBoard& gameboard, const FigureColor& figureColor);
 	static bool isCheckmate(GameBoard& gameboard, const FigureColor& figureColor);
 	static bool isStalemate(GameBoard& gameboard, const FigureColor& figureColor);
-	static bool isCastlingPossible(GameBoard& gameboard, Step& step);
 
 private:
 	static bool isCorrectStep(GameBoard& gameboard, const FigureColor& figureColor);
